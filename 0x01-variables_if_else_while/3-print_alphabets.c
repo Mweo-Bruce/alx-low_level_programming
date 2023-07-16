d0d982df6edb1917529401cfca47e0522054919d
Mweo-Bruce <mweomalupande@gmail.com>
2023-07-17 01:34:04 +0300

    C Language Code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
 /**
  * main - Prints the alphabet in lowercase and then in uppercase
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,7 @@
 /**
  * main - Prints the alphabet except q and e
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
