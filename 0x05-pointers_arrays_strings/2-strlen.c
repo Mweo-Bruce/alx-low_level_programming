7680658b5e4bf72673425119b62503e0ab72c285
Mweo-Bruce <mweomalupande@gmail.com>
2023-07-24 23:29:05 +0300

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strlen - checks the length of a string
+ * @s: string to store characters
+ * @len: int to store value
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		long++;
+		s++;
+	}
+
+	return(len);
+}
